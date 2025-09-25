4d850df22aa78dd530035e2582c1b5085216ea58
Carlos Flores Hernandez <cfloher085@g.educaaand.es>
2025-09-25 13:45:37 +0200

    Nuevos cambios

--- src/main/java/Laboral/CalculaNominas.java
+++ src/main/java/Laboral/CalculaNominas.java
@@ -3,11 +3,14 @@
  */
 
 package Laboral;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
  * @author usuario
- */
+ */ 
 public class CalculaNominas {
     
     
@@ -28,9 +31,34 @@ public class CalculaNominas {
         Empleado2 = new Empleado ("Ada Lovelace", "32000031R", 'F');
          
         
-        Nomina nomina1;
+        Nomina nomina1 = new Nomina();
         
-        nomina1 = new Nomina();
+        BufferedReader br = new BufferedReader(new FileReader("empleados.txt"));
+        String linea;
+        
+        while ((linea = br.readLine()) != null){
+            String[] partes = linea.split(",");
+            
+            int categoria = Integer.parseInt(partes[0]);
+            int anyos = Integer.parseInt(partes[1]);
+            String nombre = partes[2];
+            String dni = partes[3];
+            char sexo = partes[4].charAt(0);
+            
+            
+            /*
+            int categoria, int anyos, String nombre, String dni, char sexo
+            */
+            Empleado Empleado3;
+            Empleado3 = new Empleado (categoria, anyos, nombre, dni, sexo);
+            
+            
+            /*
+            System.out.println("Este es la nomina del empleado3: " + nomina1.sueldo(Empleado3));
+            */
+            System.out.println("Esto es lo ultimo que hemos hecho: " + Empleado3.toString());
+            
+        }
         
         
         escribe(Empleado1, nomina1.sueldo(Empleado1));
@@ -43,9 +71,18 @@ public class CalculaNominas {
         escribe(Empleado2, nomina1.sueldo(Empleado2));
         
         
+        
+        br.close(); 
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+            
         }catch(DatosNoCorrectosException emple){
             System.out.println("Los datos del empleado no son correctos");
         }
+        
+        
+        
         
     }
     
